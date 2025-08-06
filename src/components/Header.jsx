b165d67a517b32cadf11839e1ea2cb25fdb3f590
Sandra atef <https://github.com/Sandra-atef25>
2025-08-06 15:08:41 +0300

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import ReactQuizLogo from '../assets/quiz-logo.png';
+function Header(){
+    return <header>
+        <img src={ReactQuizLogo} alt="Quiz logo displaced"/>
+        <h1>ReactQuiz</h1>
+    </header>
+}
+export default Header;
